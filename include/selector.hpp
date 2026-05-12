fe60707cd261f36fb2fa474278205ddaf3d3b238
yakukhinak <yakukhinak@gmail.com>
2026-05-11 17:06:01 -0700

    Selector done - seperate files/organized - angle reset work - no x y reset

---
+++ include/selector.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+void ez_screen_task();
+void ez_template_extras();
+void selector_control();
+//void Distance_print();
--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -6,6 +6,10 @@
 extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
+inline pros::Distance front_right(17);
+inline pros::Distance front_left(20);
+inline pros::Distance left_s(3);
+inline pros::Distance right_s(12);
 
 // inline pros::Motor intake(1);
 // inline pros::adi::DigitalIn limit_switch('A');
